73dbf2f7107fa7a292a0cd54b856a5d2f93c0636
Valentin Deleplace <deleplace@google.com>
2021-02-03 10:26:55 +0100

    MetaRefCard conversation, part 1.

---
+++ static/script.js
@@ -0,0 +1,54 @@
+class SMS extends HTMLElement {
+    constructor(sender) {
+        super();
+        this.attachShadow({mode: 'open'});
+
+        const who = sender || this.getAttribute('who');
+        const what = this.innerHTML;
+        const classes = this.getAttribute('class') || "";
+        const again = this.getAttribute('again') !== null;
+
+        // Create (nested) div elements
+        const wrapper = document.createElement('div');
+        wrapper.setAttribute('class',classes);
+        wrapper.classList.add('wrapper');
+        if(again)
+            wrapper.classList.add('again');
+        wrapper.setAttribute('who', who);
+        const avatar = wrapper.appendChild(document.createElement('span'));
+        avatar.setAttribute('class','avatar');
+        avatar.setAttribute('tabindex', 0);
+        // Insert avatar from defined attribute or default avatar
+        const img = avatar.appendChild(document.createElement('img'));
+        img.src = this.hasAttribute('img') ? this.getAttribute('img') : `static/img/${who}.jpeg`;
+
+        const textMessage = wrapper.appendChild(document.createElement('div'));
+        textMessage.setAttribute('class','bubble');
+        textMessage.innerHTML = what;
+
+        // Apply external styles to the shadow dom
+        const linkElem = document.createElement('link');
+        linkElem.setAttribute('rel', 'stylesheet');
+        linkElem.setAttribute('href', 'static/style.css');
+
+        // Attach the created elements to the shadow dom
+        this.shadowRoot.appendChild(linkElem);
+        this.shadowRoot.appendChild(wrapper);
+    }
+}
+
+customElements.define('sms-bubble', SMS);
+
+class SMSValentin extends SMS {
+    constructor() {
+        super('valentin');
+    }
+}
+customElements.define('sms-valentin', SMSValentin);
+
+class SMSAnkur extends SMS {
+    constructor() {
+        super('ankur');
+    }
+}
+customElements.define('sms-ankur', SMSAnkur);
